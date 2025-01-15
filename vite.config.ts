4cc0bf6b83208a8a57cba962a527f6a7003efc74
Sergei S <NGC7891@gmail.com>
2025-01-16 02:08:07 +0600

    fix: поправлен конфиг vite для работы с css модулями

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    modules: {
+      // Настройки CSS модулей
+      localsConvention: 'camelCase',
+      scopeBehavior: 'local',
+      generateScopedName: '[name]_[local]_[hash:base64:5]',
     },
   },
 })
